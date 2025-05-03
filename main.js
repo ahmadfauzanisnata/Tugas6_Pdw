531c89eb18a322b82616907807dccc8a3cc21f95
ahmadfauzanisnata <fauzanisnata56@gmail.com>
2025-05-03 20:39:19 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function video(){
+    alert("ini video One Direction");
+});
+
+audio.addEventListener("click", function audio(){
+    alert("ini Audio One Direction");
+});
+
+gambar.addEventListener("click", function gambar(){
+    alert("ini gambar One Direction");
+});
